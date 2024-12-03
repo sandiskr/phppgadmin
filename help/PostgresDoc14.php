66c9d8d36a34ed6524c750e3077df854fc331226
sandiskr <sandiskr@gmail.com>
2024-12-03 03:55:08 +0200

    Update PostgresDoc14.php

--- help/PostgresDoc14.php
+++ help/PostgresDoc14.php
@@ -6,8 +6,8 @@
  * $Id: PostgresDoc84.php,v 1.3 2008/11/18 21:35:48 ioguix Exp $
  */
 
-include('./help/PostgresDoc13.php');
+include('./help/PostgresDoc83.php');
 
-$this->help_base = sprintf($GLOBALS['conf']['help_base'], 'devel');
+$this->help_base = sprintf($GLOBALS['conf']['help_base'], '14');
 
 ?>

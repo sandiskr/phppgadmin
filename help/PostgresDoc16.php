95c6e6b9e4380a36304d74a806e81d9560c099d6
sandiskr <sandiskr@gmail.com>
2024-12-03 03:54:43 +0200

    Create PostgresDoc16.php

---
+++ help/PostgresDoc16.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Help links for PostgreSQL 16 documentation
+ *
+ */
+
+include('./help/PostgresDoc83.php'); //include 83, because all other files loop-reverse that without any changes
+
+$this->help_base = sprintf($GLOBALS['conf']['help_base'], '16');
+
+?>
